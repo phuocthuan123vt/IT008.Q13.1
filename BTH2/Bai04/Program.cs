d5ee1ed87654fe66c967106bd701bb845a5c3efe
phuocthuan123vt <24521739@gm.uit.edu.vn>
2025-10-22 12:10:24 +0700

    Chinh sua code bai 4

--- BTH2/Bai04/Program.cs
+++ BTH2/Bai04/Program.cs
@@ -10,6 +10,28 @@ namespace Bai04
     {
         static void Main(string[] args)
         {
+            cPhanSo ps1 = new cPhanSo();
+            Console.WriteLine("Nhap phan so 1:");
+            ps1.Nhap();
+            cPhanSo ps2 = new cPhanSo();
+            Console.WriteLine("Nhap phan so 2:");
+            ps2.Nhap();
+            Console.WriteLine("Phan so 1: ");
+            ps1.Xuat();
+            Console.WriteLine("Phan so 2: ");
+            ps2.Xuat();
+            Console.WriteLine("Tong hai phan so: ");
+            cPhanSo psTong = ps1 + ps2;
+            psTong.Xuat();
+            Console.WriteLine("Hieu hai phan so: ");
+            cPhanSo psHieu = ps1 - ps2;
+            psHieu.Xuat();
+            Console.WriteLine("Tich hai phan so: ");
+            cPhanSo psTich = ps1 * ps2;
+            psTich.Xuat();
+            Console.WriteLine("Thuong hai phan so: ");
+            cPhanSo psThuong = ps1 / ps2;
+            psThuong.Xuat();
             cDanhSachPhanSo dssp = new cDanhSachPhanSo();
             dssp.NhapDanhSach();
             dssp.RutGonTatCa();
